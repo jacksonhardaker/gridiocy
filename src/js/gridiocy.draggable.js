8d70182c618371bc98e2b66f47755ddadc4b6db9
Jackson <jackson.hardaker@gmail.com>
2018-07-10 23:21:16 -0400

    Remove _ from variable names

--- src/js/gridiocy.draggable.js
+++ src/js/gridiocy.draggable.js
@@ -1,43 +1,44 @@
 import { resizeToFit } from './gridiocy.src.js';
 const draggable = {};
 
-let _startX;
-let _startY;
-let _startWidth;
-let _startHeight;
-let _contentBlock;
-let _startingRowSpan;
-let _startingColumnSpan;
-let _maxColumns;
-let _thresholdWidth;
+let startX;
+let startY;
+let startWidth;
+let startHeight;
+let contentBlock;
+let maxColumns;
+let thresholdWidth;
+let gridiocyGrid;
 
-draggable.init = function (handle, maxColumns) {
+draggable.init = function (handle, columns) {
     handle.addEventListener('mousedown', __initResize, false);
-    _maxColumns = maxColumns;
+    maxColumns = columns;
 };
 
 function __initResize(e) {
-    _contentBlock = e.target.parentElement;
-    _thresholdWidth = _contentBlock.parentElement.parentElement.offsetWidth / _maxColumns;
+    contentBlock = e.target.parentElement;
+    gridiocyGrid = contentBlock.parentElement.parentElement;
+    thresholdWidth = gridiocyGrid.offsetWidth / maxColumns;
 
-    _startX = e.clientX;
-    _startY = e.clientY;
-    _startWidth = _contentBlock.offsetWidth;
-    _startHeight = _contentBlock.offsetHeight;
-    _startingColumnSpan = _contentBlock.parentElement.dataset.columnSpan;
-    _startingRowSpan = _contentBlock.parentElement.dataset.rowSpan;
+    gridiocyGrid.style.gridTemplateRows = new Array(50).fill("1fr").join(' ');
+    //grid-template-rows: ${ new Array(50).fill("1fr").join(' ')};
 
-    _contentBlock.style.zIndex = 999;
+    startX = e.clientX;
+    startY = e.clientY;
+    startWidth = contentBlock.offsetWidth;
+    startHeight = contentBlock.offsetHeight;
+
+    contentBlock.style.zIndex = 999;
 
     window.addEventListener('mousemove', __resizeGridItem, false);
     window.addEventListener('mouseup', __finishResize, false);
 }
 
 function __resizeGridItem(e) {
-    _contentBlock.style.width = `${_startWidth - (_startX - e.clientX)}px`;
-    _contentBlock.style.height = `${_startHeight - (_startY - e.clientY)}px`;
+    contentBlock.style.width = `${startWidth - (startX - e.clientX)}px`;
+    contentBlock.style.height = `${startHeight - (startY - e.clientY)}px`;
 
-    __isContentLargerThanContainer(_contentBlock);
+    __isContentLargerThanContainer(contentBlock);
 }
 
 function __finishResize(e) {
@@ -45,23 +46,21 @@ function __finishResize(e) {
     window.removeEventListener('mousemove', __resizeGridItem, false);
     window.removeEventListener('mouseup', __finishResize, false);
 
-    _contentBlock.style.zIndex = 1;
-    resizeToFit(_contentBlock);
+    gridiocyGrid.style.gridTemplateRows = 'auto';
+    contentBlock.style.zIndex = 1;
+    resizeToFit(contentBlock);
 
 }
 function __isContentLargerThanContainer(contentBlock) {
 
     // Width
-    let exceedsWidth = (_thresholdWidth * (Number(contentBlock.parentElement.dataset.columnSpan) + 0.5)) < contentBlock.offsetWidth;
-    let smallerWidthThan = (_thresholdWidth * (Number(contentBlock.parentElement.dataset.columnSpan) - 0.5)) > contentBlock.offsetWidth;
-    //let exceedsHeight = (contentBlock.parentElement.offsetHeight * 1.5) < contentBlock.offsetHeight;
+    let exceedsWidth = (thresholdWidth * (Number(contentBlock.parentElement.dataset.columnSpan) + 0.5)) < contentBlock.offsetWidth;
+    let smallerWidthThan = (thresholdWidth * (Number(contentBlock.parentElement.dataset.columnSpan) - 0.5)) > contentBlock.offsetWidth;
 
     contentBlock.parentElement.dataset.columnSpan = exceedsWidth ? Number(contentBlock.parentElement.dataset.columnSpan) + 1 : contentBlock.parentElement.dataset.columnSpan;
     contentBlock.parentElement.dataset.columnSpan = smallerWidthThan ? Number(contentBlock.parentElement.dataset.columnSpan) - 1 : contentBlock.parentElement.dataset.columnSpan;
-    //contentBlock.parentElement.dataset.rowSpan = exceedsHeight ? Number(contentBlock.parentElement.dataset.rowSpan) + 1 : contentBlock.parentElement.dataset.rowSpan;
 
     contentBlock.parentElement.style.gridColumn = `auto / span ${contentBlock.parentElement.dataset.columnSpan}`;
-    //contentBlock.parentElement.style.gridRow = `auto / span ${contentBlock.parentElement.dataset.rowSpan}`;
 
     // Height
     let exceedsHeight = (200 * (Number(contentBlock.parentElement.dataset.rowSpan) + 0.5)) < contentBlock.offsetHeight;
