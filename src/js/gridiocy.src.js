06b3e0e4706d34cc50ae595c06332fde5cf94f90
Jackson <jackson.hardaker@gmail.com>
2018-07-12 11:20:45 -0400

    Initiate virtual grid

--- src/js/gridiocy.src.js
+++ src/js/gridiocy.src.js
@@ -1,7 +1,11 @@
 import resizable from './gridiocy.resizable.js';
 import draggable from './gridiocy.draggable.js';
 import { setAttributes } from './gridiocy.utils.js';
+import virtualGrid from './gridiocy.virtual.js';
+import uniqid from '../../node_modules/uniqid/index.js';
 const gridiocy = {};
+
+console.log(virtualGrid);
 
 let options;
 let gridiocyGrid;
@@ -19,6 +23,10 @@ gridiocy.initialize = function (indentifier, opt) {
     // Wrap content
     Array.from(document.getElementsByClassName('gridiocy-item')).forEach((item, index) => {
 
+        // Add identifier class
+        let indentifier = uniqid();
+        item.classList.add(`gridiocy-item-${indentifier}`);
+
         // Wrap inner HTML with content wrapper
         item.innerHTML = `<div class="gridiocy-item-content">${item.innerHTML}</div>`;
 
@@ -27,7 +35,10 @@ gridiocy.initialize = function (indentifier, opt) {
         let rowPos = Math.ceil((index + 1) / options.columns);
 
         item.style.gridArea = `${rowPos} / ${columnPos} / span 1 / span 1`;
-        setAttributes(item, { 'data-column-span': 1, 'data-row-span': 1, 'data-column-position': columnPos, 'data-row-position': rowPos });
+        setAttributes(item, { 'data-grid-id': indentifier, 'data-column-span': 1, 'data-row-span': 1, 'data-column-position': columnPos, 'data-row-position': rowPos });
+
+        // Initiate virtual grid.
+        virtualGrid.add(indentifier, rowPos, columnPos, 1, 1);
     });
 
     recalculateRowMajorOrder();
@@ -61,14 +72,14 @@ function getColumnWidth() {
 }
 
 function toggleAutoPositioning() {
-    Array.from(document.getElementsByClassName('gridiocy-item')).forEach((item, index) => {
+    Array.from(document.getElementsByClassName('gridiocy-item')).forEach((item) => {
         item.style.gridArea = `auto / auto / span ${item.dataset.rowSpan} / span ${item.dataset.columnSpan}`;
         //setAttributes(item, { 'data-column-span': 1, 'data-row-span': 1, 'data-column-position': columnPos, 'data-row-position': rowPos });
     });
 }
 
 function recalculateRowMajorOrder() {
-    Array.from(document.getElementsByClassName('gridiocy-item')).forEach((item, index) => {
+    Array.from(document.getElementsByClassName('gridiocy-item')).forEach((item) => {
         let zeroBasedRowPosition = Number(item.dataset.rowPosition) - 1;
         item.style.order = (zeroBasedRowPosition * options.columns) + Number(item.dataset.columnPosition);
     });
