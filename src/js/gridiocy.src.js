b10bdcd872b9b9947a84d6df9d337f09d3ccd807
Jackson <jackson.hardaker@gmail.com>
2018-07-12 10:07:29 -0400

    Implement row-major ordering

--- src/js/gridiocy.src.js
+++ src/js/gridiocy.src.js
@@ -23,22 +23,23 @@ gridiocy.initialize = function (indentifier, opt) {
         item.innerHTML = `<div class="gridiocy-item-content">${item.innerHTML}</div>`;
 
         // Add default styles and data attributes
-        let columnPos = index % options.columns + 1 ;
+        let columnPos = index % options.columns + 1;
         let rowPos = Math.ceil((index + 1) / options.columns);
 
-        item.style.gridArea = `${ rowPos } / ${ columnPos } / span 1 / span 1`;
+        item.style.gridArea = `${rowPos} / ${columnPos} / span 1 / span 1`;
         setAttributes(item, { 'data-column-span': 1, 'data-row-span': 1, 'data-column-position': columnPos, 'data-row-position': rowPos });
     });
+
+    recalculateRowMajorOrder();
 
     // If resizable
     if (options.resizable) {
         Array.from(document.getElementsByClassName('gridiocy-item-content')).forEach(item => {
-            
-
             resizable.init(item, options.columns);
         });
     }
 
+    // If draggable
     if (options.draggable) {
         Array.from(document.getElementsByClassName('gridiocy-item-content')).forEach(item => {
             draggable.init(item, options.columns);
@@ -61,9 +62,16 @@ function getColumnWidth() {
 
 function toggleAutoPositioning() {
     Array.from(document.getElementsByClassName('gridiocy-item')).forEach((item, index) => {
-        item.style.gridArea = `auto / auto / span ${ item.dataset.rowSpan } / span ${ item.dataset.columnSpan }`;
+        item.style.gridArea = `auto / auto / span ${item.dataset.rowSpan} / span ${item.dataset.columnSpan}`;
         //setAttributes(item, { 'data-column-span': 1, 'data-row-span': 1, 'data-column-position': columnPos, 'data-row-position': rowPos });
     });
 }
 
-export { gridiocy, resizeToFit, getColumnsCount, getColumnWidth, toggleAutoPositioning };
+function recalculateRowMajorOrder() {
+    Array.from(document.getElementsByClassName('gridiocy-item')).forEach((item, index) => {
+        let zeroBasedRowPosition = Number(item.dataset.rowPosition) - 1;
+        item.style.order = (zeroBasedRowPosition * options.columns) + Number(item.dataset.columnPosition);
+    });
+}
+
+export { gridiocy, resizeToFit, getColumnsCount, getColumnWidth, toggleAutoPositioning, recalculateRowMajorOrder };
