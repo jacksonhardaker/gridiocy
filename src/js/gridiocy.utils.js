dfcbe981b688ae2af790ecda601f49b423a71235
Jackson <jackson.hardaker@gmail.com>
2018-07-17 12:10:35 -0400

    Add css updates

--- src/js/gridiocy.utils.js
+++ src/js/gridiocy.utils.js
@@ -8,10 +8,14 @@ function applyVirtualGridToDOM(obj) {
     if (obj) {
         let element = document.getElementsByClassName(`gridiocy-item-${obj.id}`)[0];
 
+        // Apply Data-attributes
         element.dataset.columnSpan = obj.columnSpan;
         element.dataset.rowSpan = obj.rowSpan;
         element.dataset.columnPosition = obj.column;
         element.dataset.rowPosition = obj.row;
+
+        // Apply css changes
+        element.style.gridArea = `${obj.row} / ${obj.column} / span ${obj.rowSpan} / span ${obj.columnSpan}`;
     }
     else {
         virtualGrid.getList().forEach(applyVirtualGridToDOM);
